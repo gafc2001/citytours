3584641f2a92e97e0d667ba6334c7852ab266cf1
david alexander <davidalexd1234@gmail.com>
2021-05-08 23:15:26 -0500

    cambios david 3

--- resources/views/destinos/destino.blade.php
+++ resources/views/destinos/destino.blade.php
@@ -3,15 +3,16 @@
 @section('title','destinos')
 
 @section('content')
+
        <h1>Directorio de destinos  aqui</h1> 
-       <ul>
+       
 
               @foreach ($destinos as $destino)
-              <li>DESTINO: {{$destino-> name}} </li>
+              <li>DESTINO:{{$destino-> name}} </li>
               <a href={{route('Mostrardestinos.show',$destino-> id)}}>seleccionar destino</a>
                   
               @endforeach
-       </ul>
+       
        {{$destinos->links()}}
 @endsection
 
