1c5acc8f20492cbe6e55ff7b2aef1ad11dc2dfe8
david alexander <davidalexd1234@gmail.com>
2021-05-08 20:45:58 -0500

    cambios destino

--- app/Http/Controllers/DestinoController.php
+++ app/Http/Controllers/DestinoController.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Destino;
+use App\Models\Subdestino;
 use Illuminate\Http\Request;
 
 class destinoController extends Controller
 {
     public function index(){
-        return view('destinos.destino');
+        $destinos= Destino::paginate();
+
+        return view('destinos.destino',compact('destinos'));
 
     }
     public function show($destino){
         //tambien asi se pone 
         //compact('destino') = ['destino'=>$destino]
-        return view('destinos.Seleccion',['destino'=>$destino]);
+        
+        $Subdestinos= Subdestino::where('id_destination',$destino)->get();
+
+        
+        return view('destinos.Seleccion',['destino'=>$destino],['subdestinos'=>$Subdestinos]);
     }
 }
--- resources/views/destinos/destino.blade.php
+++ resources/views/destinos/destino.blade.php
@@ -4,5 +4,15 @@
 
 @section('content')
        <h1>Directorio de destinos  aqui</h1> 
+       <ul>
+
+              @foreach ($destinos as $destino)
+              <li>{{$destino-> name}} </li>
+              <a href="destinos/{{$destino-> id}}">seleccionar destino</a>
+                  
+              @endforeach
+       </ul>
+
+       {{$destinos->links()}}
 @endsection
 
